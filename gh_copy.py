2fff430ac016e6796928fb6f40ed02bbbe9aba81
Jack Lynch <jacklynch@wharnal.com>
2016-07-18 22:36:21 -0500

    now ignores commits older than the head of the mirror

--- gh_copy.py
+++ gh_copy.py
@@ -1,7 +1,7 @@
 from lxml import html
 import requests
 import argparse
-from datetime import datetime, timedelta
+from datetime import datetime
 from git import Repo, Actor
 
 def main():
@@ -23,19 +23,25 @@ def main():
     repo = Repo(args.repo_dir)
     assert not repo.bare
 
-    index = repo.index
+    start_date = datetime.fromtimestamp(0)
 
+    #making some dangerous assumptions here
+    if len(repo.heads) > 0:
+        start_date = datetime.fromtimestamp(repo.heads.master.commit.authored_date)
+
+    index = repo.index
     author = Actor(args.name, args.email)
 
     for contrib in contribs:
         for i in range(contrib.count):
-            commit = index.commit('', author=author, committer=author, author_date=contrib.date.isoformat())
-            assert commit.type == 'commit'
+            if contrib.date > start_date:
+                commit = index.commit('', author=author, committer=author, author_date=contrib.date.isoformat())
+                assert commit.type == 'commit'
 
 class DayContribs:
     def __init__(self, element):
         self.count = int(element.get('data-count'))
-        self.date = datetime.strptime(element.get('data-date'), '%Y-%m-%d') + timedelta(hours=12)
+        self.date = datetime.strptime(element.get('data-date'), '%Y-%m-%d')
 
 if __name__ == '__main__':
     main()
